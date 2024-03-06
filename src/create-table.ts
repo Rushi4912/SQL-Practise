3ea8fb8052b1cd324a7eafe797e40663625a01d5
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-03-06 23:49:23 +0530

    chnage

--- src/create-table.ts
+++ src/create-table.ts
@@ -1,13 +1,13 @@
 import { getClient } from "./utils";
 
-async function createTable() {
-    const createUserTableQuery = `
-        CREATE TABLE users (
-            id SERIAL PRIMARY KEY,
-            email VARCHAR(255) UNIQUE NOT NULL,
-            password VARCHAR(255) NOT NULL
-        );
-    `;
+// async function createTable() {
+//     const createUserTableQuery = `
+//         CREATE TABLE users (
+//             id SERIAL PRIMARY KEY,
+//             email VARCHAR(255) UNIQUE NOT NULL,
+//             password VARCHAR(255) NOT NULL
+//         );
+//     `;
 
     const client = await getClient();
 
