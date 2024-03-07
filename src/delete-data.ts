9b3d5d82fbb85b13787cf985af0fdcf7c9a3ff21
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-03-07 22:19:41 +0530

    change

--- src/delete-data.ts
+++ src/delete-data.ts
@@ -3,7 +3,7 @@ import { getClient } from "./utils";
 async function deleteTodo(todoId: number) {
     const client = await getClient();
     
-    const deleteTodoText = 'DELETE FROM todos WHERE id = $1';
+    // const deleteTodoText = 'DELETE FROM todos WHERE id = $1';
     await client.query(deleteTodoText, [todoId]);
     
     console.log(`Todo with ID ${todoId} deleted!`);
