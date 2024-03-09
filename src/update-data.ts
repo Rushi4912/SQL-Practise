17909139961ccb8ca5a0dfd5496933ac5ae58fdc
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-03-09 22:40:23 +0530

    Change

--- src/update-data.ts
+++ src/update-data.ts
@@ -3,8 +3,8 @@ import { getClient } from "./utils";
 async function updateTodo(todoId: number) {
     const client = await getClient();
     
-    // const updateTodoText = 'UPDATE todos SET done = $1 WHERE id = $2';
-    // await client.query(updateTodoText, [true, todoId]);
+    const updateTodoText = 'UPDATE todos SET done = $1 WHERE id = $2';
+    await client.query(updateTodoText, [true, todoId]);
     
     console.log(`Todo with ID ${todoId} updated to done!`);
 }
